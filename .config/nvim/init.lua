d0d82b8c16b5210133105ddb5bf960a53a84f04a
Yqnk <yan.bertile@gmail.com>
2024-02-25 04:28:03 +0100

    tab

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,9 @@
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false -- use tab instead of whitespaces
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
