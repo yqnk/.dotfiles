e731541336e56e369866f19c09a241c77fad8e04
Yqnk <yan.bertile@gmail.com>
2024-03-13 03:08:23 +0100

    theme

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "ayu_dark",
-  theme_toggle = { "ayu_dark", "ayu_dark" },
+  theme = "everblush",
+  theme_toggle = { "everblush", "everblush" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
