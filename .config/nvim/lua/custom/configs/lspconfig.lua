a89e1f52affdb88a47f7b56ed3f74f02ad5a9a2b
Yqnk <yan.bertile@gmail.com>
2024-03-20 13:18:44 +0100

    no more lsp errors

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,15 +3,8 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
--- lspconfig.lua_ls.setup {
---   on_attach = function(client, _)
---     -- Opt out of semantic token highlighting.
---     client.server_capabilities.semanticTokensProvider = nil
---   end,
--- }
-
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "lua_ls" }
+local servers = { "html", "cssls", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -19,3 +12,18 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.lua_ls.setup {
+  on_attach = function(client, _)
+    --Opt out of semantic token highlighting.
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+}
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
