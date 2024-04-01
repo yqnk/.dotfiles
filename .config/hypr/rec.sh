d117ce04c124e408717c79353c5d6f9adb0c09d3
Yqnk <yan.bertile@gmail.com>
2024-04-01 23:02:29 +0200

    rec

---
+++ .config/hypr/rec.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env zsh
+
+current_date=$(date +'%Y-%m-%d_%H:%M:%S')
+records_dir="$HOME/documents/records"
+mkdir -p $records_dir
+wf-recorder -b 0 -c libx264 -p b=5M -f $records_dir/recording_$current_date.mp4
