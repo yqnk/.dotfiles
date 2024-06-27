eceb861e75c06653ec94808ac01ffddf75beeabd
Yqnk <yan.bertile@gmail.com>
2024-06-27 23:56:53 +0200

    fix edit 0 at the end of scripts

--- .config/hypr/rec.sh
+++ .config/hypr/rec.sh
@@ -4,3 +4,5 @@ current_date=$(date +'%Y-%m-%d_%H:%M:%S')
 records_dir="$HOME/documents/records"
 mkdir -p $records_dir
 wf-recorder -b 0 -c libx264 -p b=5M -g "$(slurp)" -f $records_dir/recording_$current_date.mp4
+
+exit 0
